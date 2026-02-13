1ad56997d050a9953995434c9d2f2739f8bb9e71
Bharath Bolloju <22tq1a6659@siddhartha.co.in>
2026-02-13 13:29:30 +0530

    Updated the Landing Page Components

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -4,24 +4,24 @@ import Features from './components/Features';
 import Obeservation from './components/Obeservation';
 import Interviewer from './components/Interviewer';
 import Working from './components/Working';
+import Footer from './components/Footer';
 const Landing = () => {
   return (<>
-    <div className='h-[100vh] w-[100vw] flex items-center flex-col'>
-      <div className='w-[80vw]'> 
-       <Hero />
-      <Features/>
+    <div className=' w-full flex items-center flex-col'>
+      <div className='w-[80vw]'>
+        <Hero />
+        <Features />
       </div>
-      <div className='w-full'>
-      <Obeservation />
-      
-      <Interviewer />
-      <Working/>
-
+      <div className='w-full flex flex-col items-center'>
+        <Obeservation />
+        <Interviewer />
+        <Working />
+        <Footer />
       </div>
     </div>
   </>
-   
+
   )
 }
 
-export default Landing ;
+export default Landing;
